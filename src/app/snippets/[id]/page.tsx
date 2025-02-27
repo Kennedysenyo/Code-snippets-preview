b41e89023a1790b3b20ee334ac4756315a5e285e
Kennedysenyo <kennedysenyo@gmail.com>
2025-02-27 19:59:11 +0000

    added delete functionality

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound, } from "next/navigation";
 import Link from "next/link"
 import { db } from "@/db";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: {
@@ -20,7 +21,7 @@ export default async function SnippetShowPage({params}: SnippetShowPageProps) {
     return notFound();
   }
 
-  console.log(params);
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id)
   return  <div>
     <div className="flex m-4 justify-between items-center">
       <h1 className="text-xl font-bold">{snippet.title}</h1>
@@ -31,7 +32,9 @@ export default async function SnippetShowPage({params}: SnippetShowPageProps) {
         >
           Edit
         </Link>
-        <button className="p-2 border rounded">Delete</button>
+        <form action={deleteSnippetAction}>
+          <button className="p-2 border rounded">Delete</button>
+        </form>
       </div>
     </div>
     <pre className="p-3 border rounded bg-gray-200 border-gray-200">
